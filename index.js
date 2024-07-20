9f38f8d9189cbfa2ad74ffe58e60f7387dcd32c4
rishadsinu <rishadsinu2233@gmail.com>
2024-07-20 16:19:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express()
+
+app.set('view engine','ejs')
+
+
+
+
+
+
+
+
+app.listen(3030,()=>{
+    console.log(("litsening to the server on http://localhost:3030"));
+})
+
